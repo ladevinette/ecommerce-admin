98d5ce02fdfdc1d605e9c1022246ce7906a880b0
Bartek <bartekszalowski59@gmail.com>
2023-07-03 12:49:43 +0200

    made clerk auth + dashboard setup

---
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,16 @@
+import prismadb from "@/lib/prismadb";
+
+interface DashboardpageProps {
+  params: { storeId: string };
+}
+
+const Dashboardpage: React.FC<DashboardpageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return <div>Active store: {store?.name}</div>;
+};
+
+export default Dashboardpage;
